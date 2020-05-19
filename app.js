e34f80065e27bd230cb2f50be443b309f38200d2
NBESS <nicholasdbess@gmail.com>
2020-05-19 17:02:44 -0400

    adds chat app with basic functionality

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const socketio = require('socket.io');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = socketio(http);
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -17,12 +18,11 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('chat message', (msg) =>{
+    io.emit('chat message', msg);
   });
-});
+})
+
 
 http.listen(PORT, () => {
   console.log(`Chat is running: Listening on http://localhost:${PORT}`);
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,17 @@
 $(document).ready(() => {
-  const socket = io();
+  let socket = io();
+  const $messages = $('#messages');
+
+  $('.chat-form').submit(function (e) {
+    e.preventDefault();
+    socket.emit('chat message', $('.chat-input').val());
+    $('.chat-input').val('');
+  })
+
+  socket.on('chat message', function (msg) {
+    $messages.children().last().removeClass('text-success')
+    $newMessage = $(`<li class="list-group-item text-success">${msg}</li>`);
+    $messages.append($newMessage);
+    $('#messages').animate({ scrollTop: $messages.prop('scrollHeight') }, 200);
+  })
 });
